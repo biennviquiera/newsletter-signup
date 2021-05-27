82b5195f98f2dd211e59c2002882b7d3c02cf843
Bienn Viquiera <biennviquiera@gmail.com>
2021-05-27 12:25:19 -0700

    Configured body parser to work with signup page

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const request = require('request');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/signup.html');
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body.fName);
+})
+
 app.listen(port, () => {
   console.log("Server is running on port " + port);
 })
